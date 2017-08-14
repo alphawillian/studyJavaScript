2bd841ea1dfa5e4bed188723829570fe906800ac
alphawillian <1099763365@qq.com>
2017-08-14 22:27:05 +0800

    array方法扩展
    prototype的arguments 应该用this

--- js3/6_algorithm.js
+++ js3/6_algorithm.js
@@ -14,45 +14,46 @@ Array.prototype.init = function(){
 	}
 }
 Array.prototype.dimensionsArrInit = function(){
-	return arguments;
-//	var arr = arguments[0];
-//	var currentLen = arr[0];
-//	var ansArr = new Array(arr[0]);
-//	ansArr.init();
-//	if(arr.length == 1){
-//		return ansArr;
-//	}else{
-//		arr.shift();
-//		for (var i = 0; i < currentLen; i++) {
-//			if(i == 0){
-//				ansArr[i] = dimensionsArrInit(arr);
-//			}else{
-//				ansArr[i] = ansArr[i - 1];
-//			}
-//		}
-//		return ansArr;
-//	}
+//	return this;
+	var arr = this;
+	var currentLen = arr[0];
+	var ansArr = new Array(arr[0]);
+	ansArr.init();
+	if(arr.length == 1){
+		return ansArr;
+	}else{
+		arr.shift();
+		for (var i = 0; i < currentLen; i++) {
+			if(i == 0){
+				ansArr[i] = arr.dimensionsArrInit();
+			}else{
+				ansArr[i] = ansArr[i - 1];
+			}
+		}
+		return ansArr;
+	}
 }
 
 function dimensionsArrInit1(){
-//	var arr = arguments[0];
-//	var currentLen = arr[0];
-//	var ansArr = new Array(arr[0]);
-//	ansArr.init();
-//	if(arr.length == 1){
-//		return ansArr;
-//	}else{
-//		arr.shift();
-//		for (var i = 0; i < currentLen; i++) {
-//			if(i == 0){
-//				ansArr[i] = dimensionsArrInit(arr);
-//			}else{
-//				ansArr[i] = ansArr[i - 1];
-//			}
-//		}
-//		return ansArr;
-//	}
+	var arr = arguments[0];
+	var currentLen = arr[0];
+	var ansArr = new Array(arr[0]);
+	ansArr.init();
+	if(arr.length == 1){
+		return ansArr;
+	}else{
+		arr.shift();
+		for (var i = 0; i < currentLen; i++) {
+			if(i == 0){
+				ansArr[i] = dimensionsArrInit1(arr);
+			}else{
+				ansArr[i] = ansArr[i - 1];
+			}
+		}
+		return ansArr;
+	}
 }
-var test = [2,3];
+var test = [4,2,2,3];
 console.log(test.dimensionsArrInit());
+//console.log(dimensionsArrInit1([2,2,3]));
 
