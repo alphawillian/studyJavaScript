80085bb9cc77930b92b08ad789df352830ad2f28
alphawillian <1099763365@qq.com>
2017-08-14 20:30:24 +0800

    多维数组初始化成功
    但是js原生有待改进

--- js3/6_algorithm.js
+++ js3/6_algorithm.js
@@ -13,43 +13,46 @@ Array.prototype.init = function(){
 		this[i] = 0;
 	}
 }
-
-var arr = ["10","0001","1111001","1","0"];
-var m = 5;
-var n = 3;
-var arr0 = 0
-var arr1 = [arr0,arr0,arr0];
-var arr2 = [arr1,arr1];
-var arr3 = [arr2,arr2];
-console.log(arr3);
-dimensionsArrInit(2,2,3);
-
-function dimensionsArrInit(){
-	var dimensions = arguments.length;
-//	var arr = new Array(dimensions);
-	var arr = new Array();
-//	primaryArr = new Array(arguments[dimensions-1]);
-//	primaryArr.init();
-//	console.log(primaryArr);
-//	console.log(arguments);
-//	console.log(arguments[0]);
-	arr[0] = 0;
-	for(var i = dimensions; i > 0; i--){
-		arr[i] = new Array(arguments[dimensions - i]);
-		arr[i].init();
-//		if(i = 1){
-//			arr[0] = 0;
+Array.prototype.dimensionsArrInit = function(){
+	return arguments;
+//	var arr = arguments[0];
+//	var currentLen = arr[0];
+//	var ansArr = new Array(arr[0]);
+//	ansArr.init();
+//	if(arr.length == 1){
+//		return ansArr;
+//	}else{
+//		arr.shift();
+//		for (var i = 0; i < currentLen; i++) {
+//			if(i == 0){
+//				ansArr[i] = dimensionsArrInit(arr);
+//			}else{
+//				ansArr[i] = ansArr[i - 1];
+//			}
 //		}
-		for(var j = 0; j < arguments[dimensions - i]; j++){
-			arr[i][j] = arr[i-1];
-//			console.log(arr[i-1]);
-		}
-	}
-	console.log(arr);
+//		return ansArr;
+//	}
 }
-function foo(n){
-	if(n = 0){
-		return 0;
-	}
-	return 0;
+
+function dimensionsArrInit1(){
+//	var arr = arguments[0];
+//	var currentLen = arr[0];
+//	var ansArr = new Array(arr[0]);
+//	ansArr.init();
+//	if(arr.length == 1){
+//		return ansArr;
+//	}else{
+//		arr.shift();
+//		for (var i = 0; i < currentLen; i++) {
+//			if(i == 0){
+//				ansArr[i] = dimensionsArrInit(arr);
+//			}else{
+//				ansArr[i] = ansArr[i - 1];
+//			}
+//		}
+//		return ansArr;
+//	}
 }
+var test = [2,3];
+console.log(test.dimensionsArrInit());
+
