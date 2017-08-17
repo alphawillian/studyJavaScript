edda2f6b792e26ed3961144c70e453631f345a6f
alphawillian <1099763365@qq.com>
2017-08-17 17:37:41 +0800

    多维数组初始化1

--- js3/6_algorithm.js
+++ js3/6_algorithm.js
@@ -20,17 +20,17 @@ Array.prototype.dimensionsArrInit = function(){
 	var ansArr = new Array(arr[0]);
 	ansArr.init();
 	if(arr.length == 1){
-		return ansArr;
+		return ansArr.slice(0);
 	}else{
 		arr.shift();
 		for (var i = 0; i < currentLen; i++) {
 			if(i == 0){
-				ansArr[i] = arr.dimensionsArrInit();
+				ansArr[i] = arr.dimensionsArrInit().slice(0);
 			}else{
-				ansArr[i] = ansArr[i - 1];
+				ansArr[i] = ansArr[i - 1].slice(0).slice(0);
 			}
 		}
-		return ansArr;
+		return ansArr.slice(0);
 	}
 }
 
@@ -38,6 +38,7 @@ function dimensionsArrInit1(){
 	var arr = arguments[0];
 	var currentLen = arr[0];
 	var ansArr = new Array(arr[0]);
+	console.log(ansArr);
 	ansArr.init();
 	if(arr.length == 1){
 		return ansArr;
@@ -56,6 +57,8 @@ function dimensionsArrInit1(){
 var test = [2,2,2];
 var test1 = test.dimensionsArrInit();
 console.log(test);
+test1[0][0][1] = 1;
+test1[0][1][1] = 2;
 console.log(test1);
 //console.log(dimensionsArrInit1([2,2,3]));
 
--- js3/7_algorithm.js
+++ js3/7_algorithm.js
@@ -37,30 +37,27 @@ function count(str){
 function findMaxForms(arr, m, n){
 //	var dp = [arr.length+1, m+1, n+1];
 //	dp = dp.dimensionsArrInit();
-    var dp = [[[0]]];
-	for(var n1 = 0; n <= 6; n++){
-		for(var n2 = 0; n2 <= 6; n2++){
-			for(var n3 = 0; n3 <= 4; n3++){
-				dp[n1][n2][n3] = 0;
-			}
-		}
-	}
-	console.log(dp);
-	return 0;
+    var dp = [[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],
+			[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],
+		    [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],
+		    [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],
+		    [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]],
+		    [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]];
+	
 	for (var i = 1; i <= arr.length; i++){
 		var cost = count(arr[i - 1]);
 		for (var j = 0; j <= m; j++){
 			for (var k = 0; k <= n; k++){
 				if(j >= cost[0] && k >= cost[1]){
 					dp[i][j][k] = Math.max(dp[i - 1][j][k],dp[i - 1][j - cost[0]][k - cost[1]] + 1);
-					console.log(dp[i][j][k]);
+//					console.log(dp[i][j][k]);
 				}else{
 					dp[i][j][k] = dp[i - 1][j][k];
 				}
 			}
 		}
 	}
-//	return dp[arr.length][m][n];
-	return dp;
+	return dp[arr.length][m][n];
+//	return dp;
 }
 console.log( findMaxForms(arr,m,n) );
